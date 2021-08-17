cb0f32c71aba1d57fbf856e7fc1343cb62977074
Shreya991512 <88672128+Shreya991512@users.noreply.github.com>
2021-08-18 00:33:43 +0530

    Day3 mini project submitted

---
+++ Day3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Cab {
+    int fare;
+    int d;
+
+    public Cab(){
+        fare = 40;
+    }
+    public Cab(int amt){
+        fare = amt;
+    }
+}
+
+class RideCab{
+    public static void main(String[] args) {
+        int cd;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the distance of cab from user : ");
+        cd = sc.nextInt();
+        if(cd > 5){
+            Cab ob = new Cab(50 + 10 * (cd-5));
+            System.out.print("Enter the distance travelled : ");
+            ob.d = sc.nextInt();
+            ob.fare += 10 * ob.d;
+            System.out.println("Total fare: Rs"+ob.fare);
+        }
+        else{
+            Cab ob = new Cab();
+            System.out.print("Enter the distance travelled : ");
+            ob.d = sc.nextInt();
+            ob.fare += 10 * ob.d;
+            System.out.println("Total fare: Rs"+ob.fare);
+        }
+    }
+}
